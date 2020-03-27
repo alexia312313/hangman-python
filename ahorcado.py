451e322175b671913e43d504ac68811af929da7d
Alex <55158851+seitenca@users.noreply.github.com>
2020-03-27 22:40:56 +0100

    beta 2 (#1)
    done it again from scratch

--- ahorcado.py
+++ ahorcado.py
@@ -1,9 +1,18 @@
 #####################################
-# Ahorzado en python version 1-beta #
-#     Hecha por jose y alex         #
-#####################################
+#   Hangman in python version beta2  #
+#     Made by Jose and Alex          #
+######################################
 
-AHORCADO = ['''
+# Input of key word
+word = list(input("Please write a word: "))
+# Calculate the number of bars based on the numbers of letter of the word
+bar = list(len(word) * "_")
+
+
+# main function where the hangman game will be executed
+def main(win=False, failures=0, turn=0, vocal="aeiou", letter=str):
+    # variable hangman that it will show the failures that the player has
+    hangman = ['''
 
 
 
@@ -74,97 +83,52 @@ AHORCADO = ['''
      / \  |
           |
     =========''']
+    # main loop where the player will lose or win
+    while win is not True and failures != 9:
+        # try to avoid crashes
+        try:
+            turn += 1
+            correct = False
+            # check if the turn is odd or even
+            while not correct:
+                # If the turn is odd the player will have to write a vocal
+                if turn % 2 == 0:
+                    letter = input("Please write a vocal: ")
+                    if letter not in vocal:
+                        correct = False
+                    else:
+                        correct = True
+                # If the even is odd the player will have to write a consonant
+                else:
+                    letter = input("Please write a consonant: ")
+                    if letter in vocal:
+                        correct = False
+                    else:
+                        correct = True
 
-palabra = input("Escribre una palabra: ")
-win = list(palabra)
-lenword = len(palabra)
-numletras = ["_ ", "_ ", "_ ", "_ ", "_ ", "_ "]
+            # loop to check if it failed or successful
+            for i in range(0, len(word)):
+                if word[i] == letter:
+                    # if true the letter will be replace the i of the bar
+                    bar[i] = letter
+                    break
+            else:
+                failures += 1
 
-if lenword == 2:
-    numletras = ["_ ", "_ "]
-elif lenword == 3:
-    numletras = ["_ ", "_ ", "_ "]
-elif lenword == 4:
-    numletras = ["_ ", "_ ", "_ ", "_ "]
-elif lenword == 5:
-    numletras = ["_ ", "_ ", "_ ", "_ ", "_ "]
-elif lenword == 6:
-    numletras = ["_ ", "_ ", "_ ", "_ ", "_ ", "_ "]
-elif lenword == 7:
-    numletras = ["_ ", "_ ", "_ ", "_ ", "_ ", "_ ", "_ "]
-elif lenword == 8:
-    numletras = ["_ ", "_ ", "_ ", "_ ", "_ ", "_ ", "_ ", "_ "]
-elif lenword == 9:
-    numletras = ["_ ", "_ ", "_ ", "_ ", "_ ", "_ ", "_ ", "_ ", "_ "]
+            # win execution
+            if bar == word:
+                print("Congratulations you have won!")
+                win = True
 
+            # print the hangman variable
+            print(hangman[failures])
 
-def printwin():
-    if lenword == 2:
-        print(numletras[0], numletras[1])
-    elif lenword == 3:
-        print(numletras[0], numletras[1], numletras[2])
-    elif lenword == 4:
-        print(numletras[0], numletras[1], numletras[2], numletras[3])
-    elif lenword == 5:
-        print(numletras[0], numletras[1], numletras[2], numletras[3], numletras[4])
-    elif lenword == 6:
-        print(numletras[0], numletras[1], numletras[2], numletras[3], numletras[4], numletras[5])
-    elif lenword == 7:
-        print(numletras[0], numletras[1], numletras[2], numletras[3], numletras[4], numletras[5], numletras[6])
-    elif lenword == 8:
-        print(numletras[0], numletras[1], numletras[2], numletras[3], numletras[4], numletras[5], numletras[6], numletras[7])
-    elif lenword == 9:
-        print(numletras[0], numletras[1], numletras[2], numletras[3], numletras[4], numletras[5], numletras[6], numletras[7], numletras[8])
-    return
+            # print the variable bar as string
+            print(' '.join(map(str, bar)))
+
+        except ValueError:
+            print("Oops! Try it again...")
 
 
-printwin()
-fallos = 0
-
-while not numletras == win and not fallos == 9:
-    try:
-        letra = input("Escribe una letra: ")
-        if palabra[0] == letra:
-            numletras[0] = letra
-            printwin()
-        elif len(palabra) >= 2 and palabra[1] == letra:
-            numletras[1] = letra
-            printwin()
-        elif len(palabra) >= 3 and palabra[2] == letra:
-            numletras[2] = letra
-            printwin()
-        elif len(palabra) >= 4 and palabra[3] == letra:
-            numletras[3] = letra
-            printwin()
-        elif len(palabra) >= 5 and palabra[4] == letra:
-            numletras[4] = letra
-            printwin()
-        elif len(palabra) >= 6 and palabra[5] == letra:
-            numletras[5] = letra
-            printwin()
-        elif len(palabra) >= 7 and palabra[6] == letra:
-            numletras[6] = letra
-            printwin()
-        else:
-            fallos += 1
-            if fallos == 1:
-                print(AHORCADO[1])
-            elif fallos == 2:
-                print(AHORCADO[2])
-            elif fallos == 3:
-                print(AHORCADO[3])
-            elif fallos == 4:
-                print(AHORCADO[4])
-            elif fallos == 5:
-                print(AHORCADO[5])
-            elif fallos == 6:
-                print(AHORCADO[6])
-            elif fallos == 7:
-                print(AHORCADO[7])
-            elif fallos == 8:
-                print(AHORCADO[8])
-            elif fallos == 9:
-                print(AHORCADO[9])
-
-    except ValueError:
-        print("Oops! Intentelo de nuevo...")
+# Call the function main
+main()

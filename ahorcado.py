6de7b77fdb11ae5414326780bb35ed114a433125
seitenca <alex-garcia@hotmail.es>
2020-03-28 01:13:08 +0100

    beta 3 bug fixes and other improvements

--- ahorcado.py
+++ ahorcado.py
@@ -1,4 +1,4 @@
-#####################################
+######################################
 #   Hangman in python version beta2  #
 #     Made by Jose and Alex          #
 ######################################
@@ -10,7 +10,7 @@ bar = list(len(word) * "_")
 
 
 # main function where the hangman game will be executed
-def main(win=False, failures=0, turn=0, vocal="aeiou", letter=str):
+def main(failures=0, turn=0, vocal="aeiou", letter=str):
     # variable hangman that it will show the failures that the player has
     hangman = ['''
 
@@ -84,11 +84,12 @@ def main(win=False, failures=0, turn=0, vocal="aeiou", letter=str):
           |
     =========''']
     # main loop where the player will lose or win
-    while win is not True and failures != 9:
+    while failures != 9:
         # try to avoid crashes
         try:
             turn += 1
             correct = False
+            fail = True
             # check if the turn is odd or even
             while not correct:
                 # If the turn is odd the player will have to write a vocal
@@ -110,15 +111,15 @@ def main(win=False, failures=0, turn=0, vocal="aeiou", letter=str):
             for i in range(0, len(word)):
                 if word[i] == letter:
                     # if true the letter will be replace the i of the bar
+                    fail = False
                     bar[i] = letter
-                    break
-            else:
+            if fail:
                 failures += 1
 
             # win execution
             if bar == word:
                 print("Congratulations you have won!")
-                win = True
+                break
 
             # print the hangman variable
             print(hangman[failures])
